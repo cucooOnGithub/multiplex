b35e827823af4ced3f5ad59076dfe7b779cdc907
cucoo <cucoo@gmail.com>
2024-01-24 17:27:39 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # multiplex
+
+multiplex is a simple utility in python that pipes a input to a shell command.
+
+# How does it work?
+
+multiplex reads all the lines it receives from from the standard inout and applies then the bash shell command, that you specified as parameter.
+
+# Usage
+
+Provide some list via stdin, and pass the command that you wish to apply for each of that list as a input parameter to the command.
+
+Example: 
+
+```
+    cat list_of_ips.txt | multiplex "whois"
+```
+
+# Output
+
+Outputs the result of applying the command to each line
+
+# Output example
+
+```
+# cat domains.txt                        
+google.com
+apple.com
+microsoft.com
+
+# cat domains.txt | python3 multiplex.py "digg +short"
+142.250.184.14
+17.253.144.10
+20.112.250.133
+20.236.44.162
+20.70.246.20
+20.76.201.171
+20.231.239.246
+
+```
+
+# Installation
+
+Just copy the python scritp to a location in your environment path.
---
+++ multiplex.py
@@ -0,0 +1,66 @@
+#-------------------------------------------------------------------------------------
+# multiplex - Utility to apply a shell command to a list
+#-------------------------------------------------------------------------------------
+# Author    : cucoo 
+# Twitter   : @cucooOnX    
+# Github    : cucooOnGitHub#
+#-------------------------------------------------------------------------------------
+# Description : multiplex is a simple utility in python that pipe a input to a shell command.
+#               You can for instance, have a list of IP addresses and execute a shell command 
+#               on each one of them and pipe the results into other files
+#
+# Example :
+#           cat list_of_ips.txt | multiplex "whois"
+#-------------------------------------------------------------------------------------
+import subprocess
+import sys
+from subprocess import Popen, PIPE
+
+def process_input(input_lines, command):
+    processed_lines = []
+
+    for line in input_lines:
+        if command:
+            # Use subprocess to execute the specified command
+            line = line.strip()
+            if line == '':
+                continue
+
+            command2 = (command + ' ' + line).split(' ')
+            #print(command2)
+            p = Popen(command2, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+            output, err = p.communicate(b"input data that is passed to subprocess' stdin")
+            rc = p.returncode
+
+
+            # Append both stdout and stderr to the processed lines
+            processed_lines.append(output)
+            processed_lines.append(err)
+        else:
+            # If no command is provided, pass the input line to the output without transformation
+            processed_lines.append(line)
+
+    return processed_lines
+
+if __name__ == "__main__":
+    # Check if the command-line argument is provided
+    if len(sys.argv) > 1:
+        # Get the command-line argument specifying the shell command
+        command = sys.argv[1]
+    else:
+        command = None
+
+    # Read input lines from standard input
+    input_lines = sys.stdin.readlines()
+
+    # Process the input lines using the specified command or pass through if no command is provided
+    processed_lines = process_input(input_lines, command)
+
+    # Write the result to standard output
+    s = ''
+    for line in processed_lines:
+        if line != '':
+            s+= line.decode()
+
+    # sys.stdout.writelines(s)
+    print(s, flush=True)
